83e03523b64b9436ce9ebd81aba6453c3b24f942
AntonI <avmiass@bk.ru>
2023-03-16 12:38:31 +0300

    test add

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
