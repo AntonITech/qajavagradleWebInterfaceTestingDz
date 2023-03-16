dca987c3bf6b903919af4fa2a66d40108a9df818
AntonI <avmiass@bk.ru>
2023-03-16 12:03:37 +0300

    test added

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -2,11 +2,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -23,6 +26,7 @@ public class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver();
     }
 
@@ -33,8 +37,14 @@ public class CallbackTest {
     }
 
     @Test
-    void shouldTestSomething() {
+    void shouldTest() {
         driver.get("http://localhost:9999/");
-        driver.findElement().sendKeys("Сергей");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Карлов Карл");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("+79998882211");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals(expected, actual);
     }
 }
